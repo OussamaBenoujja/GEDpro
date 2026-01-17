e43def8d055499614d439bfbbe76efb3c50507a8
Oussama Benoujja <oussama@example.com>
2026-01-17 18:57:36 +0000

    Rename workflow and update Docker build steps

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,8 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors();
-
   await app.listen(process.env.PORT ?? 3000, (): void => {
     console.log('server started');
   });
